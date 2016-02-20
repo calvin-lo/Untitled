ee3a34a21d804d2c09fcf92d181213bff423be42
Albert Fung <100520898@oncampus.local>
2016-02-20 17:32:34 -0500

    basic filereader done

---
+++ Phase2/Phase2.cpp
@@ -0,0 +1,41 @@
+// Phase2.cpp : main project file.
+
+//Imports
+#include "stdafx.h"
+#include <iostream> 
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class FileReader { 
+ public:
+	string file_path; //stores file path
+	void ReadFile(); //ReadFile function
+};
+
+//ReadFile function
+void FileReader::ReadFile() { 
+	string line; //stores line of currently read string
+	ifstream file(file_path); //open ifstream
+	if (file.is_open()) {  //if file opened successfully
+		while (getline(file,line)) { //while there are lines
+			cout << line << '\n'; //output to the screen for now
+		}
+		file.close(); //close file
+	} else { //if the file is not found
+		cout << "Unable to open" << '\n';
+	}
+}
+
+int main() {
+	//new bank_accounts object
+	FileReader bank_accounts;
+	//referring to the filepath of created object
+	bank_accounts.file_path = "BankAccounts.txt";
+	//call the readfile function on filepath given above
+	bank_accounts.ReadFile();
+	//pause to read console output
+	system("pause");
+	return 0;
+}
