bfb90e6a3a02b5e6abd45a82e4ce8ff7168e23aa
Albert Fung <albert.fung@uoit.net>
2016-03-08 16:39:39 +0000

    Phase4 init

---
+++ Phase4/FileWriter.java
@@ -0,0 +1,7 @@
+//	CSCI 3060/ SOFE 3980 Course Project back end
+//	Group: Untitled
+//	Member: Calvin Lo, Albert Fung, Karan Chandwaney
+
+public class FileWriter { 
+
+}
---
+++ Phase4/Main.java
@@ -0,0 +1,9 @@
+//	CSCI 3060/ SOFE 3980 Course Project back end
+//	Group: Untitled
+//	Member: Calvin Lo, Albert Fung, Karan Chandwaney
+
+public class Main { 
+	public static void main (String args[]) { 
+		System.out.println("Hello");
+	}
+}
