4b58106b369e8f923fcc80ea4bb676c2025ca1b9
Albert Fung <albert.fung@uoit.net>
2016-03-08 17:00:47 +0000

    started

--- Phase4/FileReader.java
+++ Phase4/FileReader.java
@@ -1,10 +1,15 @@
 //	CSCI 3060/ SOFE 3980 Course Project back end
 //	Group: Untitled
 //	Member: Calvin Lo, Albert Fung, Karan Chandwaney
-//	Purpose: reads in files for the back end
+//	Purpose: reads in files for the back end and merges the Bank Account Transaction Files
+import java.util.*;
 
 public class FileReader { 
-	public FileReader() { 
+	List<String> transFiles = new ArrayList<String>();
+
+	String filePath;
+	public FileReader(String path) { 
+		filePath = path;
 
 	}
 }
