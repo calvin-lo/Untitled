d69c1296cb4af8cb940bafd00db9b1e8dbbe6b5a
Albert Fung <albert.fung@uoit.net>
2016-03-18 15:42:51 -0400

    Transaction merge finished

--- Phase4/Main.java
+++ Phase4/Main.java
@@ -1,35 +1,49 @@
 //	CSCI 3060/ SOFE 3980 Course Project back end
 //	Group: Untitled
 //	Member: Calvin Lo, Albert Fung, Karan Chandwaney
-//	Purpose: 
-//	Input files: 
-//	Output files: 
-// 	How this is intended to run: 
+//	Purpose: Main class to run the back end
+//	Input files: Transaction files, old master bank accounts file
+//	Output files: Merged transaction files, new current bank accounts file, new master bank accounts file
+// 	How this is intended to run: Compile all other classes and run this as the main class
 
-public class Main { 
+public class Main {
 
-	public static void main (String args[]) { 
+	public static void main(String args[]) {
+		// stores the directory of where the merged files will be outputed
 		String mergedPath = "";
+		// stores the directory of where the transaction files from front - end will be read in from
 		String transDir = "";
-		
-		mergedPath = "/home/admin/Code/Untitled/Phase4/";
-		transDir = "/home/admin/Code/Untitled/Phase3/";
 
-		FileReader fr = new FileReader(transDir, mergedPath);
-		fr.mergeTrans();
-		/*
-		try { 
+		// mergedPath = "/home/vm/Code/Untitled/Phase4/";
+		// transDir = "/home/vm/Code/Untitled/Phase3/";
 		
-			//mergedPath = args[0];
-			//transDir = args[1];
-			
-		} catch (Exception e) { 
-			System.out.println("No arguments specified!");
+		// try catch in case the user does not enter a valid path
+		try {
+			// first argument given is the merged path
+			mergedPath = args[0];
+			// second argument given is the transaction directory
+			transDir = args[1];
+			// if exception/error occurs, print out error messages
+		} catch (Exception e) {
+			System.out.println("No arguments specified, please enter the merge directory followed by the transaction file directory.");
+			System.out.println("Ex. java Main /home/vm/Code/Untitled/Phase4/ /home/vm/Code/Untitled/Phase3/");
 		}
+		
+		// create a new file reader
+		FileReader fr = new FileReader(transDir, mergedPath);
+		// merge the transaction files
+		fr.mergeTrans();
 
-		*/
-				
-		Transactions T = new Transactions(transDir, mergedPath);
+		// create a new file writer
+		FileWriter wr = new FileWriter("MergedBackAccountTrans.trans");
+		// get the transactions from the file reader and write them to the new file
+		wr.writeTransaction(fr.get());
+		// close the file
+		wr.close();
+
 	
+
+		Transactions T = new Transactions(transDir, mergedPath);
+
 	}
 }
