1367df96980383a92408c1fdde7f8a3324f9aeaa
Albert Fung <albert.fung@uoit.net>
2016-03-29 20:50:04 +0000

    Finished FileReader and Writer tests

--- Phase5/FileWriterTest.java
+++ Phase5/FileWriterTest.java
@@ -14,47 +14,63 @@ public class FileWriterTest {
     @Test
     public void testFileWriter() {
     	FileWriter FW = new FileWriter("Test");
+        FW.close();
     }
 
-    // Test writeTransaction method: 
+    // Test writeTransaction method: with string "moo"
     @Test
     public void testWriteTransaction() {
-    	FileWriter FW = new FileWriter("Test");
+    	FileWriter FW = new FileWriter("TestWriteTransaction");
     	
     	// set up the test data
-    	List<String> transactions = new ArrayList<String>();
-    	transactions.add("moo");
-    	FW.writeTransaction(transactions);
+    	List<String> test = new ArrayList<String>();
+    	test.add("moo");
+    	FW.writeTransaction(test);
+        FW.close();
     }
 
-    // Test writeCurrent method: 
+    // Test writeCurrent method: with account class and List of account type
     @Test
     public void testWriteCurrent() {
-    	FileWriter FW = new FileWriter("Test");
-    	
+    	FileWriter FW = new FileWriter("TestWriteCurrent");
+    	account temp = new account();
     	// set up the test data
-    	//FW.writeCurrent();
+    	List<account> test = new ArrayList<account>();
+        temp.acc_holder = "Jerry Doe Current";
+        temp.acc_num = "12345";
+        temp.acc_status = 'A';
+        temp.acc_balance = "123.33";
+        temp.acc_type = 'S';
+        temp.num_trans = 1;
+
+        test.add(temp);
+        FW.writeCurrent(test);
+        FW.close();
     }
 
-    // Test writeMaster method: 
+    // Test writeMaster method: with account class and List of account type
     @Test
     public void testWriteMaster() {
-   		FileWriter FW = new FileWriter("Test");
+   		FileWriter FW = new FileWriter("TestWriteMaster");
+        
+        // set up the test data
+    	account temp = new account();
+        List<account> test = new ArrayList<account>();
+        temp.acc_holder = "Jerry Doe Master";
+        temp.acc_num = "12346";
+        temp.acc_status = 'A';
+        temp.acc_balance = "1123.33";
+        temp.acc_type = 'N';
+        temp.num_trans = 1;
 
-    	// set up the test data
-    	//FW.writeMaster();
+        test.add(temp);
+        FW.writeMaster(test);
+        FW.close();
+        FW.close();
     }
 
-     // Test close method: 
-    @Test
-    public void testClose() {
-    	FileWriter FW = new FileWriter("Test");
-
-    	// set up the test data
-    	FW.close();
-    }
 
     public static junit.framework.Test suite() {
-        return new JUnit4TestAdapter(TransactionTest.class);
+        return new JUnit4TestAdapter(FileWriterTest.class);
     }
 }
